9e3dc611b3f406dcea0595e70c9aae45e0f414c1
anovikova <ptica-govorun@inbox.ru>
2020-04-06 03:18:09 +0300

    Подправили верстку и компоненты так, чтобы было удобнее осуществлять поиск

--- src/main.js
+++ src/main.js
@@ -8,6 +8,17 @@ import {getTripDaysTemplate} from './components/tripDays';
 import {getTripPointTemplate} from './components/tripPoint';
 
 const TRIP_COUNT = 3;
+const elem = {};
+
+const getHtmlElement = (className) => {
+  const element = document.querySelector(`.${className}`);
+
+  if (!element) {
+    throw new Error(`Элемент не найден`);
+  }
+
+  return element;
+};
 
 const renderTemplate = (container, template, place = `beforeEnd`) => {
   container.insertAdjacentHTML(place, template);
@@ -30,8 +41,8 @@ const renderTrips = (tripsCount) => {
 };
 
 renderTemplates(
-    {container: `.trip-controls h2`, render: getMenuTemplate, place: `afterEnd`},
-    {container: `.trip-controls h2~h2`, render: getfilterTemplate, place: `afterEnd`},
+    {container: `.trip-controls`, render: getMenuTemplate},
+    {container: `.trip-controls`, render: getfilterTemplate, place: `afterEnd`},
     {container: `.trip-main`, render: getTripInfoTemplate, place: `afterBegin`},
     {container: `.trip-info`, render: getTripInfoCost},
     {container: `.trip-events`, render: getSortTemplate},

79d92a890dde15a5cd1f2f970c837dfd82b8c3a5
anovikova <ptica-govorun@inbox.ru>
2020-04-12 11:08:42 +0300

    Шаблон для рендеринга разных сообщений один и тот же

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,5 +1,5 @@
-const getNoPointsTemplate = () => (
+const getMessagesTemplate = () => (
   `<p class="trip-events__msg">Click New Event to create your first point</p>`
 );
 
-export {getNoPointsTemplate};
+export {getMessagesTemplate};

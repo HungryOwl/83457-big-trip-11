156ab213e89ca060d43b166fcf1e480fef057dc3
anovikova <ptica-govorun@inbox.ru>
2020-05-25 02:03:08 +0300

    WIP

--- src/components/trip-days.js
+++ src/components/trip-days.js
@@ -23,44 +23,13 @@ const getTripEventsListTemplate = () => (
   `<ul class="trip-events__list"></ul>`
 );
 
-const getTripEventsItemTemplate = () => (
-  `<li class="trip-events__item"></li>`
-);
-
-class TripEventsList extends AbstractComponent {
-  constructor(points) {
-    super();
-    this._points = points;
-    this._element = this.getElement();
-    this._onViewChange = this._onViewChange.bind(this);
-
-    this._pointControllers = this._getPointControllers(this._onViewChange);
-  }
-
-  _onViewChange() {
-    this._pointControllers.forEach((controller) => controller.setDefaultView());
-  }
-
-  _getPointControllers(onViewChange) {
-    return this._points.map((point) => {
-      const listElement = this._getPointContainer();
-      const pointController = new PointController(listElement, onViewChange);
-      pointController.render(point);
-      this._element.append(listElement);
-      return pointController;
-    });
-  }
-
-  _getPointContainer() {
-    return createElement(getTripEventsItemTemplate());
-  }
-
+export class TripEventsList extends AbstractComponent {
   getTemplate() {
     return getTripEventsListTemplate();
   }
 }
 
-class TripDay extends AbstractComponent {
+export class TripDay extends AbstractComponent {
   constructor(dayGroup, dayNumber) {
     super();
     this._dayGroup = dayGroup;
@@ -84,34 +53,8 @@ class TripDay extends AbstractComponent {
   }
 }
 
-export default class TripDays extends AbstractComponent {
-  constructor(dayGroups) {
-    super();
-    this._tripDaysComponents = this._getTripDays(dayGroups);
-  }
-
-  _getTripDays(dayGroups) {
-    return dayGroups.map((dayGroup, day, arr) => {
-      const groupDay = (arr.length > 1) ? day + 1 : null;
-      return new TripDay(dayGroup, groupDay);
-    });
-  }
-
-  _renderTripDays() {
-    this._tripDaysComponents.forEach((tripDay) => {
-      const tripDayElem = tripDay.getElement();
-      this._element.append(tripDayElem);
-    });
-  }
-
+export class TripDays extends AbstractComponent {
   getTemplate() {
-    return getTripDaysTemplate(this._message);
-  }
-
-  getElement() {
-    super.getElement();
-    this._renderTripDays();
-
-    return this._element;
+    return getTripDaysTemplate();
   }
 }
--- src/controllers/TripEventsListController.js
+++ src/controllers/TripEventsListController.js
@@ -1,12 +1,15 @@
-import PointController from "./PointController";
-import {RenderPosition, renderTemplate} from "../utils/render";
+import {RenderPosition, renderTemplate} from '../utils/render';
+import PointController from './PointController';
+import TripDay from '../components/trip-days';
+import TripEventsList from '../components/trip-days';
 
-export default class TripEventsList {
+export class TripEventsListController {
   constructor(container) {
     this._points = [];
+    this._pointControllers = [];
     this._container = container;
+    this._tripEventsListComponent = new TripEventsList();
     this._onViewChange = this._onViewChange.bind(this);
-    this._pointControllers = this._getPointControllers(this._onViewChange);
   }
 
   _onViewChange() {
@@ -15,17 +18,57 @@ export default class TripEventsList {
 
   _getPointControllers(onViewChange) {
     return this._points.map((point) => {
-      const listElement = this._getPointContainer();
-      const pointController = new PointController(listElement, onViewChange);
+      const pointController = new PointController(this._tripEventsListComponent, onViewChange);
       pointController.render(point);
-      this._container.append(listElement);
       return pointController;
     });
   }
 
   render(points) {
     this._points = points;
+    this._pointControllers = this._getPointControllers(this._onViewChange);
 
     renderTemplate(this._container, this._tripEventsListComponent, RenderPosition.BEFOREEND);
+  }
+}
+
+export class TripDayController {
+  constructor(container) {
+    this._container = container;
+    this._tripEventsListControllers = [];
+  }
+
+  render(dayGroup, dayNumber) {
+    this._dayGroup = dayGroup;
+    this._dayNumber = dayNumber;
+    this._tripDayComponent = new TripDay(this._dayGroup, this._dayNumber);
+
+    renderTemplate(this._container, this._tripDayComponent, RenderPosition.BEFOREEND);
+  }
+}
+
+export class TripDaysController {
+  constructor(container) {
+    this._container = container;
+    this._tripDaysComponents = null;
+  }
+
+  _getDayNumber(arr, day) {
+    return (arr.length > 1) ? day + 1 : null;
+  }
+
+  _getTripDays(dayGroups) {
+    return dayGroups.map((dayGroup, day, arr) => {
+      const groupDay = this._getDayNumber(arr, day);
+      return new TripDay(dayGroup, groupDay);
+    });
+  }
+
+  render(dayGroups) {
+    this._tripDaysComponents = this._getTripDays(dayGroups);
+
+    this._tripDaysComponents.forEach((tripDayComponent) => {
+      renderTemplate(this._container, tripDayComponent, RenderPosition.BEFOREEND);
+    });
   }
 }
